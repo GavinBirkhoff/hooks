03f1794b8a6d3d434b9a08ad6b67736d41743992
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2023-06-11 17:25:02 +0800

    remove semicolon

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,38 @@
-const gulp = require('gulp');
-const babel = require('gulp-babel');
-const ts = require('gulp-typescript');
-const del = require('del');
+const gulp = require('gulp')
+const babel = require('gulp-babel')
+const ts = require('gulp-typescript')
+const del = require('del')
 
 gulp.task('clean', async function () {
-  await del('lib/**');
-  await del('es/**');
-  await del('dist/**');
-});
+  await del('lib/**')
+  await del('es/**')
+  await del('dist/**')
+})
 
 gulp.task('es', function () {
   const tsProject = ts.createProject('tsconfig.json', {
-    module: 'ESNext',
-  });
-  return tsProject.src().pipe(tsProject()).pipe(babel()).pipe(gulp.dest('es/'));
-});
+    module: 'ESNext'
+  })
+  return tsProject.src().pipe(tsProject()).pipe(babel()).pipe(gulp.dest('es/'))
+})
 
 gulp.task('cjs', function () {
   return gulp
     .src(['./es/**/*.js'])
     .pipe(
       babel({
-        configFile: '../../.babelrc',
-      }),
+        configFile: '../../.babelrc'
+      })
     )
-    .pipe(gulp.dest('lib/'));
-});
+    .pipe(gulp.dest('lib/'))
+})
 
 gulp.task('declaration', function () {
   const tsProject = ts.createProject('tsconfig.json', {
     declaration: true,
-    emitDeclarationOnly: true,
-  });
-  return tsProject.src().pipe(tsProject()).pipe(gulp.dest('es/')).pipe(gulp.dest('lib/'));
-});
+    emitDeclarationOnly: true
+  })
+  return tsProject.src().pipe(tsProject()).pipe(gulp.dest('es/')).pipe(gulp.dest('lib/'))
+})
 
-
-exports.default = gulp.series('clean', 'es', 'cjs', 'declaration');
+exports.default = gulp.series('clean', 'es', 'cjs', 'declaration')
