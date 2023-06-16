6aabb86215aa7eacf3d21b54852d551eb04b4b3b
Gavin Birkhoff <gavinbirkhoff@gmail.com>
2023-06-16 22:40:08 +0800

    Add useDeepCompareEffect test

---
+++ packages/use-deep-compare/src/__tests__/useDeepCompareEffect.test.ts
@@ -0,0 +1,33 @@
+import { renderHook, act } from '@testing-library/react'
+import { useDeepCompareEffect } from '../index'
+
+describe('useDeepCompareEffect', () => {
+  it('should call effect when dependencies change', () => {
+    const effectMock = jest.fn()
+    let dependencies = [1, { foo: 'bar' }, [3, 4]]
+
+    const { rerender } = renderHook(() => useDeepCompareEffect(effectMock, dependencies))
+
+    // Modify dependencies
+    act(() => {
+      dependencies = [2, { foo: 'baz' }, [5, 6]]
+      rerender()
+    })
+
+    expect(effectMock).toHaveBeenCalledTimes(2)
+  })
+
+  it('should not call effect when dependencies remain the same', () => {
+    const effectMock = jest.fn()
+    const dependencies = [1, { foo: 'bar' }, [3, 4]]
+
+    const { rerender } = renderHook(() => useDeepCompareEffect(effectMock, dependencies))
+
+    // Re-render with the same dependencies
+    act(() => {
+      rerender()
+    })
+
+    expect(effectMock).toHaveBeenCalledTimes(1)
+  })
+})
